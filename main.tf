3ce1e8b0a19bfd10c84cc469ab300e7edb9157ef
nreddy4 <naveenkumar9707@gmail.com>
2023-06-09 13:47:23 -0500

    ansible frontend

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test"
-  type  = "String"
+  type  = "SecureString"
   value = "helloworld"
+  key_id= "6d2f21e3-5904-44cc-b487-76a74d764359"
 }
